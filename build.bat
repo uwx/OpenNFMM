601865e27632ed9725677b07f10363e5fbe8f176
chrishansen69 <cadrekucra@gmx.com>
2016-02-02 19:29:28 -0200

    fix travis/appveyor build

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 "C:\Program Files\Java\jdk1.8.0\bin\javac" -classpath "libraries\jl1.0.1.jar" javazoom/jl/player/PausablePlayer.java
-"C:\Program Files\Java\jdk1.8.0\bin\javac" -classpath "libraries\dragshotMod.jar;libraries\easyogg.jar;libraries\jl1.0.1.jar;libraries\jogg-0.0.7.jar;libraries\jorbis-0.0.15.jar;%CD%" *.java -Xlint:deprecation
+"C:\Program Files\Java\jdk1.8.0\bin\javac" -classpath "libraries\dragshotMod.jar;libraries\easyogg.jar;libraries\jl1.0.1.jar;libraries\jogg-0.0.7.jar;libraries\jorbis-0.0.15.jar;libraries/NativeBass.jar;libraries/NativeBass-Example.jar;%CD%" *.java -Xlint:deprecation
