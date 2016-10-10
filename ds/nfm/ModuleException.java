380fb74a364bfe5ca89a9c1d8fc4807457edca63
chrishansen69 <cadrekucra@gmx.com>
2016-10-09 23:18:40 -0300

    ibxm/dsmod fixes
    (cherry picked from commit fb4495c2032658d74574fa174383b13be2635bf6)

--- ds/nfm/ModuleException.java
+++ ds/nfm/ModuleException.java
@@ -1,7 +1,12 @@
 package ds.nfm;
 
 public class ModuleException extends RuntimeException {
-   public ModuleException() {
+   /**
+     * 
+     */
+    private static final long serialVersionUID = 2007674564336027625L;
+
+    public ModuleException() {
    }
 
    public ModuleException(String message) {
